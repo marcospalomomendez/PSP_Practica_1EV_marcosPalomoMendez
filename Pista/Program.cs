0d517fcf00be5aaf8fed4f7ce2578a33511b7421
marcospalomomendez <marcos.palomomendez.25@campusfp.es>
2025-11-20 16:09:04 +0100

    Add files via upload

--- Pista/Program.cs
+++ Pista/Program.cs
@@ -22,14 +22,13 @@ namespace Pista
             int numero_canicas = int.Parse(args[0]);
             int longitudPista = int.Parse(args[1]);
 
-
-
             Thread[] canicas = new Thread[numero_canicas];
             Stopwatch[] cronos = new Stopwatch[numero_canicas];
-
+            
+            /*
             Stopwatch cronoParalelo = new Stopwatch();
             cronoParalelo.Start();
-
+            */
             Random rnd = new Random();
             List<int> pausas = new List<int>();
             for (int i = 0; i < numero_canicas; i++)
@@ -49,7 +48,9 @@ namespace Pista
                 canicas[i] = new Thread(() =>
                 {
                     cronos[dorsal] = Stopwatch.StartNew();
+
                     Carrera(longitudPista, dorsal, pausa);
+
                     cronos[dorsal].Stop();
                 });
                 canicas[i].Start();
